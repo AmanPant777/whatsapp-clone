cfac612b85a3ba8fc2d446af37c00e9190b960fe
AmanPant777 <footballeramanpt65@gmail.com>
2021-08-12 17:20:35 +0530

    add chat

--- src/comp/Chats.jsx
+++ src/comp/Chats.jsx
@@ -1,9 +1,33 @@
+import { Avatar, IconButton } from '@material-ui/core'
 import React from 'react'
 import './Chats.css'
+import SearchIcon from '@material-ui/icons/Search';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 const Chats = () => {
     return (
         <div className="chat">
-            Chats
+            <div className="chat_header">
+                <Avatar />
+                <div className="chat_headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen at</p>
+                </div>
+                <div className="chat_headerRight">
+                    <IconButton>
+                        <SearchIcon />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat_body">
+                Hi
+            </div>
         </div>
     )
 }
